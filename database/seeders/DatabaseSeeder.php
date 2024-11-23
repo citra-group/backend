11b586836b356e0fbd1e523038c21a86afe0b642
monoland <mono.socialmedia@gmail.com>
2024-11-23 14:42:09 +0700

    initial

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(\ModuleSystem\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleReference\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleHuman\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleMyCoperative\Seeders\DatabaseSeeder::class);
+        $this->call(\ModuleCoperative\Seeders\DatabaseSeeder::class);
+    }
+}
